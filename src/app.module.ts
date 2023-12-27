bb7353cddb6a7c90131f89b38f0f836dfd219f98
Christophe Simard <christophe.simard@beslogic.com>
2023-12-27 02:48:30 -0500

    add CICD

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,27 @@ import { TrackEntity } from './entities/track.entity';
 import { TrackModule } from './track/track.module';
 import { ArtistEntity } from './entities/artist.entity';
 import { AlbumEntity } from './entities/album.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [HttpModule,
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'sa',
-      password: 'Password1!',
-      database: 'model',
-      entities: [TrackEntity, ArtistEntity, AlbumEntity],
-      synchronize: true,
-      options: { encrypt: false },
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [TrackEntity, ArtistEntity, AlbumEntity],
+        synchronize: true,
+        options: { encrypt: configService.get('DATABASE_ENCRYYPT') == 'true' ? true : false },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     TrackModule,
   ],
