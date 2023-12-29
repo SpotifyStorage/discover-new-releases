e217c20da8440ec329f3831af6246493fbcbaad1
Christophe Simard <christophe.simard@beslogic.com>
2023-12-28 19:31:28 -0500

    Manually modify SQL migration files

--- src/migrations/1703793482897-rename_playcount_track_property.ts
+++ src/migrations/1703793482897-rename_playcount_track_property.ts
@@ -5,7 +5,7 @@ export class RenamePlaycountTrackProperty1703793482897 implements MigrationInter
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "playcount" DROP CONSTRAINT "FK_3d616c41f9591588de14aaee703"`);
-        await queryRunner.query(`EXEC sp_rename "model.dbo.playcount.uriTrackUri", "trackTrackUri"`);
+        await queryRunner.query(`EXEC sp_rename "SpotifyStorage.dbo.playcount.uriTrackUri", "trackTrackUri"`);
         await queryRunner.query(`ALTER TABLE "playcount" DROP COLUMN "trackTrackUri"`);
         await queryRunner.query(`ALTER TABLE "playcount" ADD "trackTrackUri" nvarchar(255)`);
         await queryRunner.query(`ALTER TABLE "playcount" ADD CONSTRAINT "FK_0c4b833f0bc7df388346df85861" FOREIGN KEY ("trackTrackUri") REFERENCES "track"("track_uri") ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -15,7 +15,7 @@ export class RenamePlaycountTrackProperty1703793482897 implements MigrationInter
         await queryRunner.query(`ALTER TABLE "playcount" DROP CONSTRAINT "FK_0c4b833f0bc7df388346df85861"`);
         await queryRunner.query(`ALTER TABLE "playcount" DROP COLUMN "trackTrackUri"`);
         await queryRunner.query(`ALTER TABLE "playcount" ADD "trackTrackUri" nvarchar(255)`);
-        await queryRunner.query(`EXEC sp_rename "model.dbo.playcount.trackTrackUri", "uriTrackUri"`);
+        await queryRunner.query(`EXEC sp_rename "SpotifyStorage.dbo.playcount.trackTrackUri", "uriTrackUri"`);
         await queryRunner.query(`ALTER TABLE "playcount" ADD CONSTRAINT "FK_3d616c41f9591588de14aaee703" FOREIGN KEY ("uriTrackUri") REFERENCES "track"("track_uri") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
