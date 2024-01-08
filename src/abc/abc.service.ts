21718ee49cdaf3de1faae57cfbf39db3f50af1e3
Mathieu <mathieuducharme86@gmail.com>
2024-01-08 17:25:46 -0500

    move artist queue receiver

--- src/abc/abc.service.ts
+++ src/abc/abc.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ArtistQueueService } from 'src/artist-queue/artist-queue.service';
 import { MinimalArtist } from 'src/artist-queue/interface/artist-minimal.interface';
 import { SpotifyPartnerService } from 'src/spotify-partner/spotify-partner.service';
 import { ArtistStatQueueMessageBody } from './dto/artist-daily.interface';
+import { ServiceBusReceiver } from '@azure/service-bus';
 
 @Injectable()
 export class AbcService {
-    //private readonly logger = new Logger(DiscoveryService.name);
+    private readonly logger = new Logger(AbcService.name);
 
     constructor(
-        private readonly artistQueueService: ArtistQueueService,
         private readonly spotifyPartnerService: SpotifyPartnerService,
+        private readonly artistQueueService: ArtistQueueService,
     ) {}
 
     async populateQueueWithArtistsUriAndAlbumcount(listOfArtist: MinimalArtist[]) {
@@ -30,6 +31,17 @@ export class AbcService {
             worldRank: data.data.artistUnion.stats.worldRank,
             date: new Date()
         }
+    }
+
+    initArtistQueueReceiver(): ServiceBusReceiver {
+        this.logger.verbose(`Initialisation of a receiver for the artist queue`)
+        return this.artistQueueService.addReceiver(async message => {
+            //this.logger.verbose(`Received a messages from the artist queue containing ${message.body.length} albums`)
+            message.body.forEach(async artist => {
+                const newMessage = await this.getArtistDailyStats(artist)
+                console.log(newMessage)
+            })
+        })
     }
 
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrackModule } from './track/track.module';
@@ -11,7 +11,6 @@ import { TokenModule } from './token/token.module';
 import { SpotifyPartnerModule } from './spotify-partner/spotify-partner.module';
 import typeorm from './config/typeorm';
 import { ArtistQueueModule } from './artist-queue/artist-queue.module';
-import { ArtistQueueService } from './artist-queue/artist-queue.service';
 import { AbcModule } from './abc/abc.module';
 import { AbcService } from './abc/abc.service';
 
@@ -42,20 +41,12 @@ import { AbcService } from './abc/abc.service';
     controllers: [],
 })
 export class AppModule implements OnModuleInit {
+
     constructor(
-        private readonly artistQueueService: ArtistQueueService,
         private readonly abcService: AbcService,
     ) { }
 
-    logger = new Logger(AppModule.name)
-
     onModuleInit() {
-        const receiver = this.artistQueueService.addReceiver(async message => {
-            //this.logger.verbose(`Received a messages from the queue containing ${message.body.length} albums`)
-            message.body.forEach(async artist => {
-                const newMessage = await this.abcService.getArtistDailyStats(artist)
-                console.log(newMessage)
-            })
-        })
+        this.abcService.initArtistQueueReceiver()
     }
 }
